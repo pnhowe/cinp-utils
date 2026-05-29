de12d13d33e4188afb67132c619493f83a0c64d6
Peter Howe <pnhowe@gmail.com>
2026-05-28 20:03:31 -0600

    fix the spelling of parameter, other misc fixes

--- cinp_utils/codegen_go.py
+++ cinp_utils/codegen_go.py
@@ -2,6 +2,8 @@ import os
 import re
 from jinja2 import Environment
 
+go_reserved_words = []
+
 # from https://github.com/golang/lint/blob/master/lint.go#L767
 commonInitialisms = ( 'acl', 'api', 'ascii', 'cpu', 'css', 'dns', 'eof', 'guid', 'html', 'http', 'https', 'id', 'ip', 'json', 'lhs', 'qps', 'ram', 'rhs', 'rpc', 'sla', 'smtp', 'sql', 'ssh', 'tcp', 'tls', 'ttl', 'udp', 'ui', 'uid', 'uuid', 'uri', 'url', 'utf8', 'vm', 'xml', 'xmpp', 'xsrf', 'xss' )
 
@@ -14,11 +16,11 @@ def fixGoName( name ):
   upper_name = name[0].upper() + name[1:]
   word_list = re.findall( '[A-Z][^A-Z]*', upper_name )
   for i in range( 0, len( word_list ) ):
-    word = word_list[i]
     if word_list[i].lower() in commonInitialisms:
       word_list[i] = word_list[i].upper()
 
   return ''.join( word_list )
+
 
 include_list = []
 prefix_list = []
@@ -352,10 +354,10 @@ func (service *{{ service|title }}) {{ model_name }}List(ctx context.Context, fi
 // {{ funcname }} calls {{ action.name }}{% if action.doc %}
 /*
 {{ action.doc }}
-*/{% endif %}{% set parm_list = [] %}{% for parm in action.paramater_list %}{{ parm_list.append( parm.name|goname + " " + parm|gotype )|default( '', True ) }}{% endfor %}
+*/{% endif %}{% set parm_list = [] %}{% for parm in action.parameter_list %}{{ parm_list.append( parm.name|goname + " " + parm|gotype )|default( '', True ) }}{% endfor %}
 func ({% if action.static %}service *{{ service|title }}{% else %}object *{{ model_name }}{% endif %}) {{ funcname }}(ctx context.Context, {{ parm_list|join(", ") }}) ({% if action.return_type %}{{ action.return_type|gotype }}, {% endif %}error) {
 	args := map[string]interface{}{
-{% for parm in action.paramater_list %}		"{{ parm.name }}": {{ parm.name|goname }},
+{% for parm in action.parameter_list %}		"{{ parm.name }}": {{ parm.name|goname }},
 {% endfor %}	}{% if action.static %}
 	uri := "{{ action.url }}"{% else %}
 	_, _, _, ids, _, err := object.cinp.GetURI().Split(object.GetURI())
